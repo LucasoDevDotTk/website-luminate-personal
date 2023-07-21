82fe6132a70b777a88f7cd1754c5a622268f2fb9
Lucas Nguyen <lucasoAlt@outlook.com>
2023-07-21 21:41:01 +0200

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
 import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), NetlifyCMS({
+  integrations: [tailwind(), NetlifyCMS({
     config: {
       backend: {
         name: 'git-gateway',
